b5901e26fde01d1492f5e224d238681d3e0db4a9
Neville Li <neville.lyh@gmail.com>
2011-03-17 23:40:09 +0800

    clean up scan.py output

--- backend/scan.py
+++ backend/scan.py
@@ -9,14 +9,7 @@ LIBRARY_DIR = '/home/public/Movies'
 
 def main():
     result = scanner.scan(LIBRARY_DIR)
-    count = 0
-    total = len(result)
     for dirpath, title, year in result:
-        count += 1
-        msg = '[%4d/%4d] %s ===> "%s"' % (count, total, os.path.basename(dirpath), title)
-        if year:
-            msg += ' (%d)' % year
-        print msg
         bigodb.add_movie(dirpath, title, year)
 
 if __name__ == '__main__':
