ba93da6adb0524ed56049d54e4316e93b47b2a82
Neville Li <neville.lyh@gmail.com>
2011-03-18 00:35:20 +0800

    remove stale library items in GC

--- backend/gc.py
+++ backend/gc.py
@@ -11,8 +11,8 @@ LIBRARY_DIR = '/home/public/Movies'
 def main():
     db = pymongo.Connection().bigodb
 
-    idmap = {
-            'Library': {},
+    libmap = {}
+    collmap = {
             'Movie': {},
             'Person': {},
             'Company': {},
@@ -20,12 +20,13 @@ def main():
 
     result = scanner.scan(LIBRARY_DIR)
     for dirpath, title, year in result:
+        libmap[dirpath] = 1
+
         item = db.Library.find_one({ 'dirpath':  dirpath })
         if not item:
             continue
 
-        idmap['Library'][item['ID']] = 1
-        idmap['Movie'][item['ID']] = 1
+        collmap['Movie'][item['ID']] = 1
 
         movie = db.Movie.find_one({ 'ID': item['ID'] })
         if not movie:
@@ -35,14 +36,21 @@ def main():
             if type(movie[key]) is list:
                 for i in movie[key]:
                     if type(i) is dict and 'type' in i and 'ID' in i:
-                        idmap[i['type']][i['ID']] = 1
+                        collmap[i['type']][i['ID']] = 1
             elif type(movie[key]) is dict and 'type' in movie[key] and 'ID' in movie[key]:
-                idmap[movie[key]['type']][movie[key]['ID']] = 1
+                collmap[movie[key]['type']][movie[key]['ID']] = 1
 
-    for coll in idmap:
+    count = 0
+    for i in db.Library.find({}, { 'dirpath': 1 }):
+        if i['dirpath'] not in libmap:
+            db.Library.remove({ 'dirpath': i['dirpath'] })
+            count += 1
+    print '[GC] Library: %d' % count
+
+    for coll in collmap:
         count = 0
         for i in db[coll].find({}, { 'ID': 1 }):
-            if i['ID'] not in idmap[coll]:
+            if i['ID'] not in collmap[coll]:
                 db[coll].remove({ 'ID': i['ID'] })
                 count += 1
         print '[GC] %s: %d' % (coll, count)
