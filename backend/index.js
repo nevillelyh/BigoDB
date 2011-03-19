3fead8b50a7ae6bc1e61414cbecb3e595e0f29e1
Neville Li <neville.lyh@gmail.com>
2011-03-19 15:11:14 +0800

    add indexes for country, language and genre

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
-db.Library.ensureIndex( { ID: 1, dirpath: 1 });
-db.Movie.ensureIndex( { ID: 1 });
-db.Person.ensureIndex( { ID: 1 });
-db.Company.ensureIndex( { ID: 1 });
+db.Library.ensureIndex({ ID: 1 } );
+db.Library.ensureIndex({ dirpath: 1 });
+db.Movie.ensureIndex({ ID: 1 });
+db.Movie.ensureIndex({ countries: 1 });
+db.Movie.ensureIndex({ genres: 1 });
+db.Movie.ensureIndex({ languages: 1 });
+db.Person.ensureIndex({ ID: 1 });
+db.Company.ensureIndex({ ID: 1 });
