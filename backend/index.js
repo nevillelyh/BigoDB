02a564a2736b52f2be2e58f3d4fbe439237c75fa
Neville Li <neville.lyh@gmail.com>
2011-03-26 03:22:06 +0800

    add index for title_vector

--- backend/index.js
+++ backend/index.js
@@ -4,5 +4,6 @@ db.Movie.ensureIndex({ ID: 1 });
 db.Movie.ensureIndex({ countries: 1 });
 db.Movie.ensureIndex({ genres: 1 });
 db.Movie.ensureIndex({ languages: 1 });
+db.Movie.ensureIndex({ title_vector: 1 });
 db.Person.ensureIndex({ ID: 1 });
 db.Company.ensureIndex({ ID: 1 });
